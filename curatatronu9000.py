fd3aba3f8c9d8dbdc8b403865212a9ceb97d74b8
alexxmatei <4lexnium@gmail.com>
2022-12-30 17:59:59 +0200

    Implement scheduled message posting

--- curatatronu9000.py
+++ curatatronu9000.py
@@ -1,8 +1,11 @@
 import os
+import pytz
 from telegram import Bot
+from datetime import time
 from dotenv import load_dotenv
 from timedate import getCurrentWeekNr
 from supabase import create_client, Client
+from telegram.ext import Updater, CallbackContext, JobQueue
 
 # The load_dotenv function will read a file named .env in the current working directory.
 # It will then load any environment variables that are defined in the file.
@@ -22,3 +25,17 @@ SUPABASE_KEY: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 print(getCurrentWeekNr())
+
+messageSchedule = time(hour=18, minute=30, second=0, tzinfo=pytz.timezone("Europe/Bucharest"))
+
+updater = Updater(CURATATRONU9000_API_TOKEN, use_context=True)
+job: JobQueue = updater.job_queue
+
+def callbackSendScheduledMessage(context: CallbackContext):
+    context.bot.send_message(chat_id=userAlId, 
+                             text='It is: ' + str(messageSchedule))
+
+jobWeekly = job.run_daily(callbackSendScheduledMessage, messageSchedule)
+
+updater.start_polling()
+updater.idle()
