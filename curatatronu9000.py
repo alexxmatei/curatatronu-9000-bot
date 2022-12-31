f9e28d5208dcfb903cbe558916442980235eb878
alexxmatei <4lexnium@gmail.com>
2022-12-31 14:16:59 +0200

    Move envar loading to different file Also restructure code

--- curatatronu9000.py
+++ curatatronu9000.py
@@ -1,35 +1,22 @@
-import os
+
 import pytz
 from telegram import Bot
 from datetime import time
-from dotenv import load_dotenv
+from data import SUPABASE_KEY, SUPABASE_URL, CURATATRONU9000_API_TOKEN, userAlId
 from timedate import getCurrentWeekNr
-from supabase import create_client, Client
 from telegram.ext import Updater, CallbackContext, JobQueue
+from supabase import create_client, Client
 
-# The load_dotenv function will read a file named .env in the current working directory.
-# It will then load any environment variables that are defined in the file.
-# These environment variables can then be accessed using the os module.
-load_dotenv()
-
-# Get the CURATATRONU9000_API_TOKEN environment variable stored in the local .env file
-CURATATRONU9000_API_TOKEN: str = os.environ['CURATATRONU9000_API_TOKEN']
-userAlId: str = os.environ['USER_AL_ID'] # do the same for USER_AL_ID
-
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 curatatronu9000Bot: Bot = Bot(token=CURATATRONU9000_API_TOKEN)
+updater = Updater(CURATATRONU9000_API_TOKEN, use_context=True)
+job: JobQueue = updater.job_queue
 
 curatatronu9000Bot.send_message(text='INIȚIERE PROTOCOL TEST...', chat_id=userAlId)
 
-SUPABASE_URL: str = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
 print(getCurrentWeekNr())
 
-messageSchedule = time(hour=18, minute=30, second=0, tzinfo=pytz.timezone("Europe/Bucharest"))
-
-updater = Updater(CURATATRONU9000_API_TOKEN, use_context=True)
-job: JobQueue = updater.job_queue
+messageSchedule = time(hour=19, minute=0, second=0, tzinfo=pytz.timezone("Europe/Bucharest"))
 
 def callbackSendScheduledMessage(context: CallbackContext):
     context.bot.send_message(chat_id=userAlId, 
---
+++ data.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+# The load_dotenv function will read a file named .env in the current working directory.
+# It will then load any environment variables that are defined in the file.
+# These environment variables can then be accessed using the os module.
+load_dotenv()
+
+CURATATRONU9000_API_TOKEN: str = os.environ['CURATATRONU9000_API_TOKEN']
+userAlId: str = os.environ['USER_AL_ID'] # do the same for USER_AL_ID
+SUPABASE_URL: str = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY: str = os.environ.get("SUPABASE_KEY")
