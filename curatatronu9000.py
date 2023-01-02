ee3f812d16b555d5c7a7efddb889e39cdbd98f84
alexxmatei <4lexnium@gmail.com>
2023-01-02 02:14:51 +0200

    Update main program to work with new files

--- curatatronu9000.py
+++ curatatronu9000.py
@@ -1,28 +1,15 @@
-
-import pytz
-from telegram import Bot
-from datetime import time
-from data import SUPABASE_KEY, SUPABASE_URL, CURATATRONU9000_API_TOKEN, userAlId
-from timedate import getCurrentWeekNr
+from exportTypes import newShiftStart
 from telegram.ext import Updater, CallbackContext, JobQueue
-from supabase import create_client, Client
+from data import CURATATRONU9000_API_TOKEN, groupFacetiBaCuratenie
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-curatatronu9000Bot: Bot = Bot(token=CURATATRONU9000_API_TOKEN)
 updater = Updater(CURATATRONU9000_API_TOKEN, use_context=True)
 job: JobQueue = updater.job_queue
 
-curatatronu9000Bot.send_message(text='INIȚIERE PROTOCOL TEST...', chat_id=userAlId)
+# TODO - Function description
+def cbSendScheduledMessage(context: CallbackContext):
+    context.bot.send_message(chat_id=groupFacetiBaCuratenie, text=newShiftStart.message)
 
-print(getCurrentWeekNr())
-
-messageSchedule = time(hour=19, minute=0, second=0, tzinfo=pytz.timezone("Europe/Bucharest"))
-
-def callbackSendScheduledMessage(context: CallbackContext):
-    context.bot.send_message(chat_id=userAlId, 
-                             text='It is: ' + str(messageSchedule))
-
-jobWeekly = job.run_daily(callbackSendScheduledMessage, messageSchedule)
+jobWeekly = job.run_daily(cbSendScheduledMessage, newShiftStart.time, newShiftStart.day)
 
 updater.start_polling()
 updater.idle()
